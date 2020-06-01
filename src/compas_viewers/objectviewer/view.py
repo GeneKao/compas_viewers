ca708d4d4a00dfb0dad64ed063e89312059380d6
Li <leochen1990@gmail.com>
2020-06-01 17:21:46 +0200

    shader experiment

--- src/compas_viewers/objectviewer/view.py
+++ src/compas_viewers/objectviewer/view.py
@@ -10,6 +10,11 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+# get shaders
+from OpenGL.GL import shaders
+# import the vertext buffer object
+from OpenGL.arrays import vbo
+
 from compas.utilities import hex_to_rgb
 from compas.utilities import rgb_to_hex
 
@@ -110,7 +115,6 @@ class View(GLWidget):
                     for item in list(self.selected):
                         self.deselect(item)
 
-
     def keyPressAction(self, key):
         if key == SHIFT:
             self.selecting = True
@@ -184,9 +188,9 @@ class View(GLWidget):
                 m.edge_instance = []
                 m.edges_instance_color = []
                 m.edge_xyz = []
-                for j,e in enumerate(edges):
+                for j, e in enumerate(edges):
 
-                    v1,v2 = e
+                    v1, v2 = e
                     m.edge_xyz.extend(m.view.xyz[v1])
                     m.edge_xyz.extend(m.view.xyz[v2])
 
@@ -196,7 +200,6 @@ class View(GLWidget):
                     m.edges_instance_color.append(_hex)
                     m.edges_instance_color.append(_hex)
                     self.intances[_hex] = {'edge': e, 'mesh': i}
-
 
         self.buffers = []
         for node in self.nodes:
@@ -209,7 +212,7 @@ class View(GLWidget):
             edges_instance_color = flist(hex_to_rgb(c) for c in node.edges_instance_color)
             instance_color = flist(hex_to_rgb(node.instance_color) for key in node.view.xyz)
 
-            if hasattr(node.view,'vertices_color'):
+            if hasattr(node.view, 'vertices_color'):
                 vertices_color = flist(vc for vc in node.view.vertices_color)
             else:
                 vertices_color = flist(hex_to_rgb('#000000') for key in node.view.vertices)
@@ -218,13 +221,12 @@ class View(GLWidget):
 
             edges_color = flist(hex_to_rgb(edges_color) for key in edges)
             faces_color = flist(hex_to_rgb(face_color) for key in node.view.xyz)
-            faces_color_back = flist(hex_to_rgb(face_color) for key in node.view.xyz)            
-
+            faces_color_back = flist(hex_to_rgb(face_color) for key in node.view.xyz)
 
             vertices_selected_color = flist(hex_to_rgb('#999900') for key in node.view.vertices)
             edges_selected_color = flist(hex_to_rgb('#aaaa00') for key in edges)
             faces_selected_color = flist(hex_to_rgb('#ffff00') for key in node.view.xyz)
-            faces_selected_color_back = flist(hex_to_rgb('#ffff00') for key in node.view.xyz)      
+            faces_selected_color_back = flist(hex_to_rgb('#ffff00') for key in node.view.xyz)
 
             self.buffers.append({
 
@@ -252,6 +254,8 @@ class View(GLWidget):
                 'v': len(vertices),
                 'e': len(edges),
                 'f': len(faces)})
+
+        self.create_shader()
 
     def draw_buffers(self):
         if not self.buffers:
@@ -304,8 +308,8 @@ class View(GLWidget):
 
             glDisableClientState(GL_COLOR_ARRAY)
             glDisableClientState(GL_VERTEX_ARRAY)
-        
-        
+
+        self.draw_shader()
 
     def draw_rotation_center(self):
 
@@ -314,7 +318,7 @@ class View(GLWidget):
             self.sphere_buffer = {
                 'xyz': self.make_vertex_buffer(self.camera.target),
                 'vertices': self.make_index_buffer([0]),
-                'vertices.color': self.make_vertex_buffer([0.7,1,0], dynamic=True),
+                'vertices.color': self.make_vertex_buffer([0.7, 1, 0], dynamic=True),
                 'v': 1
             }
 
@@ -380,7 +384,7 @@ class View(GLWidget):
         size = self.size()
         ratioH = int(self.GL_height / size.height())
         ratioW = int(self.GL_width / size.width())
-        instance = instance[::ratioH,::ratioW,:]
+        instance = instance[::ratioH, ::ratioW, :]
 
         instance = np.flip(instance, 0)
         self.instance_map = instance
@@ -388,10 +392,107 @@ class View(GLWidget):
         glClearColor(1.0, 1.0, 1.0, 1.0)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+    def create_shader(self):
+        # create a vertex and fragment shader
+        # apparently verterx shaders only need
+        # to return a gl_Position, an fragment
+        # shaders only need to return a color
+        vshader = '''#version 120
+        varying vec3 ec_pos;
+
+        void main(){
+            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            ec_pos = (gl_ModelViewMatrix * gl_Vertex).xyz;
+        }
+        '''
+        fshader = '''#version 120
+        varying vec3 ec_pos;
+        void main(){
+
+            vec3 light = vec3(0.5, 0.2, 1.0);
+            light = normalize(light);
+
+            vec4 color = vec4( 1,0,0,1 );
+            vec3 ec_normal = normalize(cross(dFdx(ec_pos), dFdy(ec_pos)));
+
+            float dProd = max(0.0,
+                    dot(ec_normal, light));
+
+            color = vec4(ec_normal, 1);
+            //color = vec4(dProd, dProd, dProd, 1);
+            gl_FragColor = color;
+            
+        }
+
+        
+        '''
+        # compile our shaders
+        VERTEX_SHADER = shaders.compileShader(vshader, GL_VERTEX_SHADER)
+        FRAGMENT_SHADER = shaders.compileShader(fshader, GL_FRAGMENT_SHADER)
+        self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+        # create a vbo (stored actual data for our triangles)
+
+        # for node in self.nodes:
+        #     xyz = np.array(node.view.xyz)[:3]
+
+        # vertices = np.array([[1.78149199, 0.32750149, 0.],
+        #                      [1.32750149, 1.21850801, 0.],
+        #                      [2.21850801, 1.67249851, 0.]],'f')
+        # print(vertices.dtype)
+        
+        # # vertices = np.array([[1.78, 0.327, 0.],
+        # #                      [1.32, 1.21, 0.],
+        # #                      [2.21, 1.67, 0.]],'f')
+
+        # # vertices = xyz
+
+        vertices = np.concatenate([np.array(node.view.xyz, dtype=np.float32) for node in self.nodes])
+        faces = np.concatenate([np.array(node.view.faces) for node in self.nodes])
+        print(faces.shape)
+        
+        vertex_buffer = [[ [node.view.xyz[index] for index in face] for face in node.view.faces] for node in self.nodes]
+        vertex_buffer = np.array(vertex_buffer, dtype=np.float32)
+        print(vertex_buffer.shape)
+        vertex_buffer = np.reshape(vertex_buffer,(vertex_buffer.shape[0]*vertex_buffer.shape[1]*vertex_buffer.shape[2],vertex_buffer.shape[3]))
+        self.vertex_buffer = vertex_buffer
+
+        print(np.array(vertex_buffer).shape)
+
+        self.vbo = vbo.VBO(vertex_buffer)
+
+    def draw_shader(self):
+        '''render the geometry of the scene'''
+        # tell opengl to use our shader
+        shaders.glUseProgram(self.shader)
+        try:
+            # bind data into gpu
+            self.vbo.bind()
+            try:
+                # tells opengl to access vertex once
+                # we call a draw function
+                glDisable(GL_POLYGON_SMOOTH)
+                glEnableClientState(GL_VERTEX_ARRAY)
+                
+                # point at our vbo data
+                glVertexPointerf(self.vbo)
+                # actually tell opengl to draw
+                # the stuff in the VBO as a series
+                # of triangles
+                glDrawArrays(GL_TRIANGLES, 0, self.vertex_buffer.shape[0])
+            finally:
+                # cleanup, unbind the our data from gpu ram
+                # and tell opengl that it should not
+                # expect vertex arrays anymore
+                self.vbo.unbind()
+                glDisableClientState(GL_VERTEX_ARRAY)
+        finally:
+            # stop using our shader
+            shaders.glUseProgram(0)
 
 # ==============================================================================
 # Main
 # ==============================================================================
+
 
 if __name__ == '__main__':
 
